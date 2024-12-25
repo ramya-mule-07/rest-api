fd278e6717e0254445bfb5d63a8f6afac9d048e3
ramya <muleramya7@gmail.com>
2024-12-25 23:36:08 +0530

    intial codes

--- restapi_app/models.py
+++ restapi_app/models.py
@@ -1,3 +1,39 @@
 from django.db import models
 
 # Create your models here.
+# ... Imports Section ... #
+from django.contrib.auth.models import AbstractBaseUser
+from django.contrib.auth.models import PermissionsMixin
+# ....................... #
+
+
+class UserProfile(AbstractBaseUser, PermissionsMixin):
+    """
+    Represents a "user profile" inside out system. Stores all user account
+    related data, such as 'email address' and 'name'.
+    """
+
+    email = models.EmailField(max_length=255, unique=True)
+    name = models.CharField(max_length=255)
+    is_active = models.BooleanField(default=True)
+    is_staff = models.BooleanField(default=False)
+
+    objects = UserProfileManager() # type: ignore
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ['name']
+
+    def get_full_name(self):
+        """Django uses this when it needs to get the user's full name."""
+
+        return self.name
+
+    def get_short_name(self):
+        """Django uses this when it needs to get the users abbreviated name."""
+
+        return self.name
+
+    def __str__(self):
+        """Django uses this when it needs to convert the object to text."""
+
+        return self.email
