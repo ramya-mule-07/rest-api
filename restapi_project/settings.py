fb68234db49164fe5979d501fb1d03944eadb739
ramya <muleramya7@gmail.com>
2024-12-26 21:26:31 +0530

    added custom user profile model,manager and migrations

--- restapi_project/settings.py
+++ restapi_project/settings.py
@@ -39,7 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'rest_framework',
     'rest_framework.authtoken',
-    'rest_app',
+    'restapi_app',
 ]
 
 MIDDLEWARE = [
@@ -121,3 +121,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+
+AUTH_USER_MODEL = 'restapi_app.Userprofile'
